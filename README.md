e6269e9b1a6b9797c9ea0dd3f8442f209fd4552c
Alex Craig <alexandercraig@protonmail.com>
2018-09-25 23:45:24 -0700

    Created three notebooks, explored methods to access GSheets - settled on the gspread library

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 ### Maintained by: Alex Craig
 ***
 Python module and Jupyter Notebook that pull from Google Sheets, submit jobs to remote host, and return dataframes for analysis
-
+***
+## How to:
+* Make a CLUSTERHOST file and supply the name of the remote computer you wish to run jobs on
---
+++ voyager.py
@@ -0,0 +1,18 @@
+"""
+Voyager - Module for accessing subsets of GSheet data in a Jupyter Notebook, exchanging the data and running jobs on a remote cluster in a top-level way, and performing analysis in the notebook post-job.
+
+Alexander Craig
+alexander.craig@schrodinger.com
+"""
+
+# filename = "CLUSTERHOST"
+
+
+class FileManager:
+    """
+    Generic context manager to handle credentials
+    """
+    def __init__(self):
+        pass
+    
+    def
